677d7be5bceda249cc35f0c06ad8212f24118c10
Dave Evans <devans@fastmail.com>
2021-08-19 10:46:29 -0400

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cd ./src
 /usr/bin/rsync -a public/* root@www.daveevans.us:/var/www/www.daveevans.us/
 /usr/bin/ssh root@www.daveevans.us /usr/bin/systemctl reload apache2
 
-/usr/bin/rm -rf ./src/public
+/usr/bin/rm -rf ./public
