1a80a54c9efb1e76462b112ded611eaeffb58908
Dave Evans <devans@fastmail.com>
2021-10-11 21:56:11 -0400

    update action

--- cicd/install_hugo.sh
+++ cicd/install_hugo.sh
@@ -1,5 +1,5 @@
 !#/usr/bin/env bash
 
-HUGO_VERSION=0.74.3
-https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz /tmp/
-tar -xf /tmp/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz -C /usr/local/bin/
+HUGO_VERSION=0.88.1
+wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz -C /usr/local/bin/
